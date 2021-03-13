a302c84577fcbdfabbec2708a93546e57bc7aee1
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2021-03-13 12:01:53 +0100

    wip

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const faker = require("faker")
 const express = require("express")
 const app = new express()
 const PORT = 8080
 
+const teams = {
+  pippo: {
+    name: "pippo",
+    password: "",
+    score: 100,
+    killedShips: [],
+    firedBullets: 10,
+    lastFiredBullet: new Date().getTime()
+  }
+}
 const field = []
 const ships = []
 
-const W = process.argv[2] || 10
-const H = process.argv[3] || 10
-const S = 1//process.argv[4] || 10
+const W = process.argv[2] || 6
+const H = process.argv[3] || 6
+const S = process.argv[4] || 10
 
 for (let y = 0; y < H; y++) {
   const row = []
@@ -23,32 +34,69 @@ for (let y = 0; y < H; y++) {
   field.push(row)
 }
 
+let id = 1
 for (let i = 0; i < S; i++) {
+  const maxHp = faker.random.number({ min: 1, max: 6 })
+  const vertical = faker.random.boolean()
+  console.log({ vertical, maxHp })
+
   const ship = {
-    id: "pino",
-    x: 3,
-    y: 3,
-    vertical: false,
-    maxHp: 4,
+    id,
+    name: faker.name.firstName(),
+    x: faker.random.number({ min: 0, max: vertical ? W - 1 : W - maxHp }),
+    y: faker.random.number({ min: 0, max: vertical ? H - maxHp : H - 1 }),
+    vertical,
+    maxHp,
     curHp: 4,
     alive: true,
-    killTeam: null
+    killer: null
   }
 
-  ships.push(ship)
-
+  let found = false
   for (let e = 0; e < ship.maxHp; e++) {
     const x = ship.vertical ? ship.x : ship.x + e
     const y = ship.vertical ? ship.y + e : ship.y
-    field[y][x].ship = ship
+    if (field[y][x].ship) {
+      found = true
+      break
+    }
+  }
+
+  if (!found) {
+    for (let e = 0; e < ship.maxHp; e++) {
+      const x = ship.vertical ? ship.x : ship.x + e
+      const y = ship.vertical ? ship.y + e : ship.y
+      field[y][x].ship = ship
+    }
+  
+    ships.push(ship)
+    id ++
   }
 }
 
-
-
 app.get("/", ({ query: { format } }, res) => {
+  const visibleField = field.map(row => row.map(cell => ({ 
+    x: cell.x,
+    y: cell.y,
+    hit: cell.hit,
+    team: cell.team,
+    ship: cell.hit ? 
+      cell.ship ? { id: cell.ship.id, name: cell.ship.name, alive: cell.ship.alive, killer: cell.ship.killer } : null 
+      : null
+  })))
+
+  const visibleShipInfo = ships.map(ship => ({
+    id: ship.id,
+    name: ship.name,
+    alive: ship.alive,
+    killer: ship.killer
+  }))
+
   if ( format === "json") {
-    res.json(field)
+    res.json({ 
+      field: visibleField,
+      ships: visibleShipInfo
+    })
   } else {
     // html format field
     res.send(`
@@ -60,6 +108,11 @@ app.get("/", ({ query: { format } }, res) => {
         table, td, th {
           border: 1px solid black;
         }
+
+        td {
+          width: 40px;
+          height: 40px;
+        }
         
         table {
           width: 100%;
@@ -70,7 +123,7 @@ app.get("/", ({ query: { format } }, res) => {
     <body>
       <table>
         <tbody>
-          ${field.map(row => `<tr>${row.map(cell => `<td>${cell.ship ? cell.ship.id : "acqua"}</td>`).join("")}</tr>`).join("")}
+          ${visibleField.map(row => `<tr>${row.map(cell => `<td>${cell.ship ? cell.ship.name : ""}</td>`).join("")}</tr>`).join("")}
         </tbody>
       </table>
     </body>
@@ -83,7 +136,22 @@ app.get("/score", (req, res) => {
   res.json([])
 })
 
-app.get("/fire", ({ query: { x, y, team } }, res) => {  
+app.signup("/signup", (req, res) => {
+  //team password
+})
+app.get("/fire", ({ query: { x, y, team, password } }, res) => {  
+  /*
+    1. segnare la cella come colpita
+    2. segnare eventualmente la nave come colpita (ridurre gli hp e verificare se e' morta)
+    3. assegnare il team sia alla cella che alla nave (eventuale)
+    4. assicurarsi che il team che chiama l'endpoint non possa chiamarlo per piu' di una volta al secondo (opzionale)
+    5. definire un punteggio conseguente all'attacco:
+      a. punteggio molto negativo se si spara fuori dal campo
+      b. punteggio 0 se acqua
+      c. punteggio negativo se spari su casella gia' colpita
+      c. punteggio positivo se spari su nave ma non la uccidi
+      d. punteggio molto positivo se spari su nave e la uccidi
+  */
   res.json({
     x, y, team
   })
