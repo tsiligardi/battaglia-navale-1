ba307d36045070aec6a98d5548022a7faaa790df
Tommaso Siligardi <tomesse@outlook.it>
2021-03-20 13:08:28 +0100

    aggiunto array killedships

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = new express()
 const PORT = 8080
 
 const teams = {
-  pippo: {
-    name: "pippo",
-    password: "",
-    score: 100,
+  test: {
+    name: "test",
+    password: "test",
+    score: 0,
     killedShips: [],
-    firedBullets: 10,
-    lastFiredBullet: new Date().getTime()
+    firedBullets: 0,
+    lastFiredBullet: 0
   }
 }
 const field = []
@@ -19,6 +19,12 @@ const ships = []
 const W = process.argv[2] || 6
 const H = process.argv[3] || 6
 const S = process.argv[4] || 10
+
+const gameStatus = {
+  active: true,
+  startTime: new Date().getTime(),
+  endTime: null
+}
 
 for (let y = 0; y < H; y++) {
   const row = []
@@ -47,8 +53,7 @@ for (let i = 0; i < S; i++) {
     y: faker.random.number({ min: 0, max: vertical ? H - maxHp : H - 1 }),
     vertical,
     maxHp,
-    curHp: 4,
-    alive: true,
+    curHp: maxHp,
     killer: null
   }
 
@@ -81,14 +86,19 @@ app.get("/", ({ query: { format } }, res) => {
     hit: cell.hit,
     team: cell.team,
     ship: cell.hit ?
-      cell.ship ? { id: cell.ship.id, name: cell.ship.name, alive: cell.ship.alive, killer: cell.ship.killer } : null
+      cell.ship ? {
+        id: cell.ship.id,
+        name: cell.ship.name,
+        alive: cell.ship.curHp > 0,
+        killer: cell.ship.killer
+      } : null
       : null
   })))
 
   const visibleShipInfo = ships.map(ship => ({
     id: ship.id,
     name: ship.name,
-    alive: ship.alive,
+    alive: ship.curHp > 0,
     killer: ship.killer
   }))
 
@@ -108,7 +118,6 @@ app.get("/", ({ query: { format } }, res) => {
         table, td, th {
           border: 1px solid black;
         }
-
         td {
           width: 40px;
           height: 40px;
@@ -123,7 +132,7 @@ app.get("/", ({ query: { format } }, res) => {
     <body>
       <table>
         <tbody>
-          ${visibleField.map(row => `<tr>${row.map(cell => `<td>${cell.ship ? cell.ship.name : ""}</td>`).join("")}</tr>`).join("")}
+          ${visibleField.map(row => `<tr>${row.map(cell => `<td>${cell.ship ? cell.ship.name : cell.hit ? "X" : ""}</td>`).join("")}</tr>`).join("")}
         </tbody>
       </table>
     </body>
@@ -133,13 +142,42 @@ app.get("/", ({ query: { format } }, res) => {
 })
 
 app.get("/score", (req, res) => {
-  res.json([])
+  res.json(Object.values(teams).map(({
+    name,
+    score,
+    killedShips,
+    firedBullets,
+    lastFiredBullet
+  }) => ({
+    name,
+    score,
+    killedShips,
+    firedBullets,
+    lastFiredBullet
+  })))
 })
 
-app.signup("/signup", (req, res) => {
-  // team password
+app.get("/signup", ({ query: { name, password } }, res) => {
+  if (!name || !password) {
+    return res.sendStatus(400)
+  }
+
+  if (teams[name]) {
+    return res.sendStatus(409)
+  }
+
+  teams[name] = {
+    name,
+    password,
+    score: 0,
+    killedShips: [],
+    firedBullets: 0,
+    lastFiredBullet: 0
+  }
+
+  res.sendStatus(200)
 })
-app.get("/fire", ({ query: { x, y, team, password } }, res) => {
+app.get("/fire", ({ query: { x, y, team: teamName, password } }, res) => {
   /*
     1. segnare la cella come colpita
     2. segnare eventualmente la nave come colpita (ridurre gli hp e verificare se e' morta)
@@ -152,9 +190,72 @@ app.get("/fire", ({ query: { x, y, team, password } }, res) => {
       c. punteggio positivo se spari su nave ma non la uccidi
       d. punteggio molto positivo se spari su nave e la uccidi
   */
+
+  if (!gameStatus.active) {
+    return res.sendStatus(400)
+  }
+
+  const team = teams[teamName]
+  if (!team) {
+    return res.sendStatus(401)
+  }
+  if (team.password !== password) {
+    return res.sendStatus(401)
+  }
+  const now = new Date().getTime()
+  if (now - team.lastFiredBullet < 1000) {
+    return res.sendStatus(408)
+  }
+
+  team.firedBullets ++
+  team.lastFiredBullet = now
+
+  let message, score
+  if (x >= W || x < 0 || y >= H || y < 0) {
+    message = "out of field"
+    score = -10
+  } else {
+    const cell = field[y][x]
+
+    if (cell.hit) {
+      message = "already hit"
+      score = -5
+    } else {
+      cell.hit = true
+      cell.team = team
+
+      if (!cell.ship) {
+        message = "water"
+        score = 0
+      } else {
+        cell.ship.curHp --
+        if (cell.ship.curHp > 0) {
+          message = "hit!"
+          score = 10
+        } else {
+          cell.ship.killer = team
+          message = "killer!"
+          score = 50
+          team.killedShips.push({
+            name: cell.ship.name,
+            id: cell.ship.id
+          })
+
+          if (ships.every(({ curHp }) => curHp === 0)) {
+            gameStatus.active = false
+            gameStatus.endTime = new Date().getTime()
+          }
+        }
+      }
+    }
+  }
+
   res.json({
-    x, y, team
+    message,
+    score
   })
+
+  team.score += score
 })
 
 app.all("*", (req, res) => {
